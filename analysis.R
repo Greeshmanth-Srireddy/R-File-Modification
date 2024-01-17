8d7cf4aed7ed137249c2b5ecd44590beb4a1010d
Greeshmanth Srireddy <greeshmanthsrireddy@gmail.com>
2024-01-16 20:20:05 -0500

    Modified analysis.R

---
+++ analysis.R
@@ -0,0 +1,10 @@
+#This is creation of Variables
+name <- "Greeshmanth Srireddy"
+age <- 40
+#This is Creation of print Method
+print(name)
+#This is Creation of Function
+multiplcation <- function(a,b){
+  print(a*b)
+}
+multiplcation(2,3)
